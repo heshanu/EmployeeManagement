2a97d4adda4d396ecc6e9fd13bc98c7c791f272d
heshanu <heshanu97@gmail.com>
2023-12-05 02:46:21 +0530

    deleted

--- frontEnd/employeemanagement/src/app/comp/add-student/add-student.component.ts
+++ frontEnd/employeemanagement/src/app/comp/add-student/add-student.component.ts
@@ -14,22 +14,12 @@ export class AddStudentComponent {
     id: null,
     name: [null, Validators.required],
     email: [null, Validators.required],
-    phoneNumber: [null, Validators.required],
-    // address2: null,
-    // city: [null, Validators.required],
-    // state: [null, Validators.required],
-    // postalCode: [null, Validators.compose([
-    //   Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-    // ],
-    // shipping: ['free', Validators.required]
+    phoneNumber: [null, Validators.required]
   });
 
- 
   onSubmit(): void {
     if (this.addressForm.valid) { 
       console.log('Form Submitted!');
-      alert('Form Submitted!');
-      //console.log(this.addressForm.value);
     }
   }
 }
