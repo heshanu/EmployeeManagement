c34835c57e8f982b5018e27c23a736be7df9997a
heshanu <heshanu97@gmail.com>
2023-12-05 01:11:39 +0530

    added structure

---
+++ backend/WebAPPEmployeeManagement/src/main/java/com/AngularSpring/WebAPPEmployeeManagement/WebAppEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.AngularSpring.WebAPPEmployeeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ backend/WebAPPEmployeeManagement/src/test/java/com/AngularSpring/WebAPPEmployeeManagement/WebAppEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AngularSpring.WebAPPEmployeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
